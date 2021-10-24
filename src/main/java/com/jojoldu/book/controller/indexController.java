4f7d7154ad6efe66d7ed5183dd67c40388699663
jun111haha <jun111haha@gmail.com>
2021-10-24 20:14:52 +0900

    v2 commit

--- src/main/java/com/jojoldu/book/controller/indexController.java
+++ src/main/java/com/jojoldu/book/controller/indexController.java
@@ -1,10 +1,12 @@
 package com.jojoldu.book.controller;
 
+import com.jojoldu.book.dto.PostsResponseDto;
 import com.jojoldu.book.service.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -22,6 +24,14 @@ public class indexController {
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 
 }
